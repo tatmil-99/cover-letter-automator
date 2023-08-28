424cb2e276932b678b31bd08c749fd83e800908b
Tatien Miller <tatmil99@gmail.com>
2023-08-28 00:30:34 -0500

    Removes data file and logic writing past jobs to binary. I did this because I developed a better strategy for replacing jobs in the letter.txt file. I read the contents of a "template file" and formatted the letter using .format() str method. I then wrote the formatted letter to a separate file. This significantly reduced complexity and length of my program.

--- letter_automator.py
+++ letter_automator.py
@@ -1,27 +1,18 @@
 from datetime import datetime
-import pickle
 
 
-def edit_letter(company, position, previous_jobs):
-    previous_company = previous_jobs[-1]['Company']
-    previous_position = previous_jobs[-1]['Position']
-
+def edit_letter(company, position):
+    template = 'template.txt'
     file = 'letter.txt'
-    content_changes = []
 
-    with open(file, 'r') as letter:
-        for line in letter.readlines():
-            if previous_company in line:
-                change = line.replace(previous_company, company)
-                content_changes.append(f'{change}')
-            elif previous_position in line:
-                change = line.replace(previous_position, position)
-                content_changes.append(f'{change}')
-            else:
-                content_changes.append(f'{line}')
+    with open(template, 'r') as letter:
+        lines = letter.readlines()
+
+    joined = ''.join(lines)
+    formatted = joined.format(company=company, position=position)
 
     with open(file, 'w') as letter:
-        letter.writelines(content_changes)
+        letter.write(formatted)
 
 
 current_datetime = datetime.now()
@@ -33,27 +24,4 @@ today = (
 company = input('What company are you applying to? ')
 position = input('What is the position? ')
 
-job_info_file = 'jobinfo.data'
-job_info_list = []
-
-try:
-    with open(job_info_file, 'rb') as read_file:
-        stored_info = pickle.load(read_file)
-
-    edit_letter(company, position, stored_info)
-
-    stored_info.append({'Company': company, 'Position': position})
-    print(stored_info)
-
-    with open(job_info_file, 'wb') as write_file:
-        pickle.dump(stored_info, write_file)
-except EOFError:
-    job_info_list.append(
-        {'Company': 'Temp Stop', 'Position': 'LAMP Developer'}
-    )
-    print(f'Initializing object: {job_info_list} as binary.')
-
-    with open(job_info_file, 'wb') as write_file:
-        pickle.dump(job_info_list, write_file)
-
-    del job_info_list
+edit_letter(company, position)
