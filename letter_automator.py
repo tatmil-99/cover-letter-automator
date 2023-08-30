9d717bdf596799b4d93d81e73809820aa3ddf849
Tatien Miller <tatmil99@gmail.com>
2023-08-30 16:55:14 -0500

    Creates `format_pdf()` which opens letter.txt and converts file to pdf.

--- letter_automator.py
+++ letter_automator.py
@@ -2,11 +2,21 @@ from datetime import datetime
 from fpdf import FPDF
 
 
-def format_pdf(letter):
+def format_pdf():
     pdf = FPDF()
     pdf.add_page()
-    pdf.set_font('Arial', 12)
-    pdf.set_margins(2.54, 2.54)
+    pdf.add_font('Arial', '', '/Users/tatienmiller/Library/Fonts/Arial.ttf', True)
+    pdf.set_font('Arial')
+    pdf.set_top_margin(2.54)
+    pdf.set_right_margin(2.54)
+
+    with open('letter.txt', 'r') as letter:
+        lines = letter.readlines()
+
+    for line in lines:
+        pdf.cell(0, 5, line, 0, 1, 'L')
+
+    pdf.output('cover_letter.pdf')
 
 
 def edit_letter(today, company, position):
@@ -22,6 +32,8 @@ def edit_letter(today, company, position):
     with open(file, 'w') as letter:
         letter.write(formatted)
 
+    # format_pdf(letter)
+
 
 current_datetime = datetime.now()
 today = (
@@ -33,3 +45,4 @@ company = input('What company are you applying to? ')
 position = input('What is the position? ')
 
 edit_letter(today, company, position)
+format_pdf()
