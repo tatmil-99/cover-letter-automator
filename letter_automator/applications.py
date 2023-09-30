e52a1a3712ba5a9c50a6840ef04a12604d622e5a
Tatien Miller <tatmil99@gmail.com>
2023-09-30 04:41:55 -0500

    restructures repo and modularizes code. also renames files respective to their functionality and changes some names for sake of brevity.

---
+++ letter_automator/applications.py
@@ -0,0 +1,56 @@
+class Job:
+    '''Represents jobs applied to'''
+
+    applications = {}
+    total = 0
+
+    def __init__(self, company, position, date):
+        self.company = company.lower()
+        self.position = position.lower()
+        self.date = date
+
+        Job.total += 1
+
+    def update_company(self, company):
+        old_company = self.company
+        self.company = company.lower()
+        new_company = self.company
+        Job.update_key(new_company, old_company)
+
+    def update_position(self, position):
+        self.position = position.lower()
+
+    @classmethod
+    def read_jobs(cls):
+        print(f'Total applications: {cls.total}')
+        for i in cls.applications:
+            print((f'company: {cls.applications[i].company}, '
+                   f'position: {cls.applications[i].position}, '
+                   f'date: {cls.applications[i].date}'))
+
+    # Need to list all applications to specific company (as tuple)?
+    @classmethod
+    def search_job(cls, job):
+        if job in cls.applications:
+            print(job)
+        else:
+            print('Not found')
+
+    @classmethod
+    def update_key(cls, new, old):
+        cls.applications[new] = cls.applications.pop(old)
+        print(cls.applications)
+
+    @classmethod
+    def store(cls, job):
+        cls.applications[job.company] = job
+        print(cls.applications)
+
+    @classmethod
+    def delete_job(cls, job):
+        try:
+            del cls.applications[job]
+            print(f'Deleting job: {job}')
+            Job.total -= 1
+        except KeyError:
+            print(f'Could not find job: {job}.')
