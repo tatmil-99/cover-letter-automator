cfe73813cf7b4e4db38441aceebc2b76ea8e62da
Tatien Miller <tatmil99@gmail.com>
2023-10-01 15:21:55 -0500

    adds program title to ArgumentParser constructor to display for ux friendly info to cli

--- letter_automator/interface.py
+++ letter_automator/interface.py
@@ -4,38 +4,14 @@ from datetime import datetime
 import applications
 
 # Need to make CRUD commands with company, position, etc., arguments.
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    '--create', help='create pdf for cover letter', action='store_true')
-parser.add_argument(
-    '--store', help='store job data', action='store_true')
-parser.add_argument(
-    '--list jobs', help='list all jobs applied to', action='store_true')
-parser.add_argument(
-    '--search', help='seaches if company exists in applications',
-    action='store_true')
+parser = argparse.ArgumentParser(prog='cover letter automator')
+
 args = parser.parse_args()
 
-# Using optional arguments here instead of positional because the data
-# needed requires custom input from the user
-if args.create and args.store:
-    current_datetime = datetime.now()
-    today = (f'{current_datetime.month}/'
-             f'{current_datetime.day}/'
-             f'{current_datetime.year}')
-    company = input('What company are you applying to? ').strip()
-    position = input('What is the position? ').strip()
 
-    letter.edit(today, company, position)
-
-    job = applications.Job(company, position, today)
-    applications.Job.store(job)
-elif args.create:
-    current_datetime = datetime.now()
-    today = (f'{current_datetime.month}/'
-             f'{current_datetime.day}/'
-             f'{current_datetime.year}')
-    company = input('What company are you applying to? ').strip()
-    position = input('What is the position? ').strip()
-
-    letter.edit(today, company, position)
+# current_datetime = datetime.now()
+# today = (f'{current_datetime.month}/'
+#          f'{current_datetime.day}/'
+#          f'{current_datetime.year}')
+# company = input('What company are you applying to? ').strip()
+# position = input('What is the position? ').strip()
