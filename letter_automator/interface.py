39c99bb862a9c43e9c2f4614929bd4b1f5850bd1
Tatien Miller <tatmil99@gmail.com>
2023-10-03 04:01:10 -0500

    creates `create` subcommand that accepts two positional arguments

--- letter_automator/interface.py
+++ letter_automator/interface.py
@@ -3,8 +3,14 @@ import argparse
 from datetime import datetime
 import applications
 
-# Need to make CRUD commands with company, position, etc., arguments.
-parser = argparse.ArgumentParser(prog='cover letter automator')
+parser = argparse.ArgumentParser(prog='cover letter automator',
+                                 description='Automate cover letters and store job related data')
+subparser = parser.add_subparsers(title='subcommands', help='CRUD operations')
+
+create_parser = subparser.add_parser(
+    'create', help='create cover letter with arguments')
+create_parser.add_argument('company', help='company name')
+create_parser.add_argument('position', help='job position')
 
 args = parser.parse_args()
 
